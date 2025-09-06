cc2c359ea24d3dcd28ed4a9690843f524cef0d88
invilso <invilsomail@gmail.com>
2025-09-06 16:19:22 +0300

    Enable Django ManifestStaticFilesStorage
    Implements file fingerprinting for static assets to improve caching and version control. This change ensures browser cache busting when static files are modified by appending a content hash to filenames.

--- arrow_rsc/settings.py
+++ arrow_rsc/settings.py
@@ -170,6 +170,8 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+
 
 
 MEDIA_URL = '/media/'
